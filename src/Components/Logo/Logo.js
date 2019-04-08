a6f4e51d4062542bd04c5e2b55be19bab43c864a
Punisa <dasha888999@mail.ru>
2019-04-08 18:59:31 +0300

    first commit

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-// import logo from './logo.png'
+import logo from './logo.png'
 import './logo.css'
 
 export default class Logo extends Component {
@@ -8,9 +8,7 @@ export default class Logo extends Component {
     }
     render() {
         return(
-            <div>
-                
-            </div>
+            <img id="logo" src={logo}/>
         )
     }
 }
